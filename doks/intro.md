f97c632ad7dd0572658e2766443b8eabc26bec74
johndmulhausen <jmulhausen@digitalocean.com>
2020-04-08 17:09:51 -0700

    Setup fix attempt

--- doks/intro.md
+++ doks/intro.md
@@ -5,3 +5,11 @@ real cloud provider, DigitalOcean. This means you'll be able to interact
 with your cluster in a real dashboard, and it'll be live on the internet!
 
 ![DigitalOcean Kubernetes](/do-product-docs/scenarios/doks/assets/doks.png)
+
+We're going to download a couple tools in this playground:
+
+- `doctl`, the DigitalOcean command-line interface
+- `kubectl`, the command-line interface for Kubernetes
+
+You may see the `curl` commands go by that grab these tools. That's normal!
+When the setup script completes its work, the screen will be cleared.
--- doks/setup.sh
+++ doks/setup.sh
@@ -1,6 +1,8 @@
-curl -sL https://github.com/digitalocean/doctl/releases/download/v1.33.1/doctl-1.33.1-linux-amd64.tar.gz | tar -xzv
+curl -sL https://github.com/digitalocean/doctl/releases/download/v1.40.0/doctl-1.40.0-linux-amd64.tar.gz | tar -xzv
 sudo mv ~/doctl /usr/local/bin
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir -p example; cd example/
 echo "ALL DONE! CONTINUE..."
+clear
